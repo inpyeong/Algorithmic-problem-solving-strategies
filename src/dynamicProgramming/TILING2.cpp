9841dba40c88033ca7fc724aaa35a4e150bfcb52
inpyeong <jinipyung@gmail.com>
2020-10-01 17:22:33 +0900

    Solve TILING2

--- src/dynamicProgramming/TILING2.cpp
+++ src/dynamicProgramming/TILING2.cpp
@@ -4,20 +4,30 @@
 using namespace std;
 
 int n;
-long long cache[101];
+int cache[101];
+
+int tiling(int width) {
+  if(width <= 1) return 1;
+
+  int& ret = cache[width];
+  if(ret != -1) return ret;
+
+  return ret = (tiling(width-2) + tiling(width-1)) % 1000000007;
+}
 
 void solve() {
-  memset(cache, 0, sizeof(cache));
+  memset(cache, -1, sizeof(cache));
   cin >> n;
 
-  cache[1] = 1;
-  cache[2] = 2;
-  for(int i = 3; i <= 100; ++i) { 
-    cache[i] = cache[i-1] + cache[i-2];
-    cache[i] %= 1000000007;
-  }
+  // cache[1] = 1;
+  // cache[2] = 2;
+  // for(int i = 3; i <= 100; ++i) { 
+  //   cache[i] = cache[i-1] + cache[i-2];
+  //   cache[i] %= 1000000007;
+  // }
 
-  cout << cache[n] << "\n";
+  // cout << cache[n] << "\n";
+  cout << tiling(n) << "\n";
 }
 
 int main() {
