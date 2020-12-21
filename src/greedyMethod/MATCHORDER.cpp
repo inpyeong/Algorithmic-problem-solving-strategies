67b585004935a9f8d63a77c3ff0febd8d1cec6af
inpyeong <jinipyung@gmail.com>
2020-12-22 03:12:05 +0900

    Solve MATCHORDER.cpp using multiset

--- src/greedyMethod/MATCHORDER.cpp
+++ src/greedyMethod/MATCHORDER.cpp
@@ -1,43 +1,39 @@
 #include <iostream>
 #include <vector>
+#include <set>
 #include <algorithm>
 
 using namespace std;
 
 int N;
 
+int order(const vector<int>& russian, const vector<int> korean) {
+  int wins = 0;
+  multiset<int> ratings(korean.begin(), korean.end());
+  for(int rus = 0; rus < N; ++rus) {
+    if(*ratings.rbegin() < russian[rus])
+      ratings.erase(ratings.begin());
+    else {
+      ratings.lower_bound(russian[rus]);
+      wins++;
+    }
+  }
+  return wins;
+}
+
 void solve() {
   cin >> N;
-  vector<pair<int, int> > russia, korea;
-
+  vector<int> russian, korean;
   int rating;
   for(int i = 0; i < N; ++i) {
     cin >> rating;
-    russia.push_back(make_pair(rating, i));
+    russian.push_back(rating);
   }
   for(int i = 0; i < N; ++i) {
     cin >> rating;
-    korea.push_back(make_pair(rating, false));
+    korean.push_back(rating);
   }
-
-  sort(russia.begin(), russia.end());
-  sort(korea.begin(), korea.end());
-
-  int answer = 0;
-  for(int rusIdx = 0; rusIdx < N; ++rusIdx)
-    for(int i = 0; i < N; ++i) {
-      int korRating = korea[i].first;
-      int rusRating = russia[rusIdx].first;
-      bool ordered = korea[i].second;
-      if(ordered) continue;
-      if(korRating >= rusRating) {
-        korea[i].second = true;
-        answer++;
-        break;
-      }
-    }
-
-  cout << answer << "\n";
+  cout << order(russian, korean) << "\n";
 }
 
 int main() {
